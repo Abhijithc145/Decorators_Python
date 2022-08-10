df0a42643f25aad19ee0014b6ece9258b3c4bf89
Abhijith <abhijithabhipgmr145@gmail.com>
2022-08-11 00:49:29 +0530

    first of decorators

--- Decorators.py
+++ Decorators.py
@@ -1,1 +1,71 @@
- sjks
+# Any callable python objects that is used to modify a function or a class 
+
+
+# function decorators and class decorators
+
+
+from unittest import result
+
+
+# def outer():
+#     x = 3
+#     def inner():
+#         y = 3
+#         result = x + y 
+        
+#         return result
+#     return inner
+
+# a = outer()
+# print(a())        
+
+
+# ======================================
+
+
+# def function():
+#     print("hi am function 1")
+
+# def fuction2(fun):
+#     print("hi am fuchtion 2")
+#     fun()
+
+# fuction2(function)       
+
+
+# ======================================
+
+
+# def all(fun):
+#     def inner():
+#         str =fun()
+#         return str.upper()
+#     return inner    
+
+# def print_str():
+#     return "Abhijith"
+
+# print(print_str())
+
+# d = all(print_str)
+# print(d())
+# ======================================
+
+
+def all(fun):
+    def inner():
+        str =fun()
+        return str.upper()
+    return inner()   
+
+@all
+def print_str():
+    return "Abhijith"
+
+print(print_str)
+
+                #   =====================| 1  |
+
+
+
+# ======================================
